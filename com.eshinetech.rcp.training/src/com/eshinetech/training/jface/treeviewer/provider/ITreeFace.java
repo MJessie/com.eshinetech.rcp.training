8789ad095e40ba3f6a1f479684a5eb975ed640b2
JavaRui <1299020954@qq.com>
2018-07-20 14:38:31 +0800

    add some code description

--- com.eshinetech.rcp.training/src/com/eshinetech/training/jface/treeviewer/provider/ITreeFace.java
+++ com.eshinetech.rcp.training/src/com/eshinetech/training/jface/treeviewer/provider/ITreeFace.java
@@ -5,10 +5,28 @@ import java.util.List;
 import org.eclipse.swt.graphics.Image;
 
 public interface ITreeFace {
+	/**显示的字符串
+	 * @return
+	 */
 	public String getName();
+    /**设置显示字符串
+     * @param name
+     */
     public void setName(String name);
+    /**设置子tree列表
+     * @param Children
+     */
     public void setChildren(List Children);
+    /**获取子tree列表
+     * @return
+     */
     public List getChildren();
+    /**获取id
+     * @return
+     */
     public int getId();
+    /**设置图片
+     * @return
+     */
     public Image getImg();
 }
